192badaf4dbde0834f608111551fa957ec0aa71a
andyeung523 <andyeung523@gmail.com>
2020-02-03 01:21:06 +0800

    rebuild

--- src/App.js
+++ src/App.js
@@ -5,24 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      div id = "hi" class="container-fluid text-center">
-        <p id="greeting"></p>
-
       Welcome to Andy's portholio.
-
-        </div>
-
+    </div>
   );
 }
-<script type="text/javascript">
-  var d = new Date();
-  var h = d.getHours();
-
-    if(h >=12){
-    h >= 18 ? document.getElementById("greeting").innerHTML = "Good evening!" : document.getElementById("greeting").innerHTML = "Good afternoon!"
-  }
-  else
-    document.getElementById("greeting").innerHTML = "Good morning!";
-</script>
-
 export default App;
