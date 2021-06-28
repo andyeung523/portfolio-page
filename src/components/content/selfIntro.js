088ce719c26f7478ac8f0adfa24c30766f12ffa3
andyeung523 <andyeung523@gmail.com>
2021-06-29 02:28:19 +0800

    update transition & transform

--- src/components/content/selfIntro.js
+++ src/components/content/selfIntro.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './content.css'
-import self_img from '../../asset/img/me_icon_3.png'
+import self_img from '../../asset/img/me_icon_2.jpeg'
 
 export default function SelfIntro() {
     return (
         <div className="flexbox " id="self-area"> 
 
             <div className="self-intro-text ">
-                <p id="self-text">
+                <p id="self-text ">
                     Hi, I am Andy Yeung. <br/>
                     Welcome to my website
 
--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,40 +1,59 @@
 import './head.css'
 
-import React from 'react'
+import React,{ useState } from 'react'
 import { FaBars } from 'react-icons/fa';
 
 
+
+
 function Head() {
+    const [checked, setChecked] = useState(false);
+    var dropDownList;
+    function onclick () {
+        console.log(checked);
+       setChecked(!checked)
+    }
+
+    if(checked==0){
+        dropDownList =<div className="dropDown">dropdown</div>;
+    }else{
+        dropDownList=<div className="dropDown"></div>;
+    }
+
+
     return (
-        <div id="head-area" className="flexBox">
-            <div id="title">
-                Andy Yeung
+        <section>
+            <div id="head-area" className="flexBox">
+                <div id="title">
+                    Andy Yeung
+                </div>
+
+                <div id="btn-div ">
+                    <button className="nav-btn">
+                        About
+                    </button>
+                    <button className="nav-btn">
+                        Background
+                    </button>
+                    <button className="nav-btn">
+                        Skill
+                    </button>
+                    <button className="nav-btn">
+                        Portfolio
+                    </button>
+                    <button className="nav-btn">
+                        Contact
+                    </button>
+                    <button className="icon neumorphy" onClick={onclick}>
+                        <FaBars className="icon" />
+                    </button>
+                </div>
+
             </div>
 
-            <div id="btn-div ">
-                <button className="nav-btn">
-                    About
-                </button>
-                <button className="nav-btn">
-                    Background
-                </button>
-                <button className="nav-btn">
-                    Skill
-                </button>
-                <button className="nav-btn">
-                    Portfolio
-                </button>
-                <button className="nav-btn">
-                    Contact
-                </button>
-                <button className="icon neumorphy" onclick="myFunction()">
-                    <FaBars className="icon"/>
-                </button>
-            </div>
+            {dropDownList}
 
-
-
-        </div>
+        </section>
     )
 }
 
